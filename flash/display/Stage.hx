d36ac39201b5f5760676d72f3d3a6d9d8014d0d2
Nickolay Grebenshikov <grebenshikov.n@gmail.com>
2014-08-05 11:33:08 +0800

    Event.STAGE_RENDERED added.

--- flash/display/Stage.hx
+++ flash/display/Stage.hx
@@ -445,8 +445,8 @@ class Stage extends DisplayObjectContainer {
 		}
 		
 		this.__renderAll ();
-        var stage_rendered = new Event("STAGE_RENDERED");
-        this.__broadcast(stage_rendered);
+        var stageRenderedEvent = new Event(Event.STAGE_RENDERED);
+        this.__broadcast(stageRenderedEvent);
 	}
 	
 	
